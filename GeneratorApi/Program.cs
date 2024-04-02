e118c1586905447a39d467e4864f31e9d27ffd14
Elad Yona <eladyona@gmail.com>
2024-04-02 16:04:56 +0300

    edit Dockerfile

--- GeneratorApi/Program.cs
+++ GeneratorApi/Program.cs
@@ -16,29 +16,34 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+var beerRecipes = new List<BeerRecipe>()
+{ 
+    new ("IPA", "A hoppy beer", "IPA", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Stout", "A dark beer", "Stout", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Pilsner", "A light beer", "Pilsner", "Lager", "Hops, Malt, Yeast", "Instructions"),
+    new ("Porter", "A dark beer", "Porter", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Saison", "A spicy beer", "Saison", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Wheat", "A wheat beer", "Wheat", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Lager", "A light beer", "Lager", "Lager", "Hops, Malt, Yeast", "Instructions"),
+    new ("Sour", "A sour beer", "Sour", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Barleywine", "A strong beer", "Barleywine", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Amber", "An amber beer", "Amber", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Blonde", "A blonde beer", "Blonde", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Brown", "A brown beer", "Brown", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Cream", "A cream beer", "Cream", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Pale Ale", "A pale ale", "Pale Ale", "Ale", "Hops, Malt, Yeast", "Instructions"),
+    new ("Red Ale", "A red ale", "Red Ale", "Ale", "Hops, Malt, Yeast", "Instructions")
 };
 
-app.MapGet("/weatherforecast", () =>
+var random = new Random();
+
+app.MapGet("/beer/recipes/generate", () =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        int index = random.Next(beerRecipes.Count);
+        return beerRecipes[index];
     })
-    .WithName("GetWeatherForecast")
     .WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
-}
+record BeerRecipe(string Name, string Description, string Style, string Type, string Ingredients, string Instructions);
